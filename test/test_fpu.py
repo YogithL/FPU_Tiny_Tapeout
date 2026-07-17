8db0cc3ee1cac951689fc358edece56aae8f0217
YogithL <lyogith@gmail.com>
2026-07-17 05:11:01 -0500

    Added base CRT

--- test/test_fpu.py
+++ test/test_fpu.py
@@ -4,6 +4,7 @@ from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles, ReadOnly
 from enum import IntEnum
 import numpy as np
 import ml_dtypes
+import vsc
 
 global acc_reg
 
@@ -64,6 +65,59 @@ def goldenModel(A, B, op, acc, acc_reg_val):
     return result_int, flag_underflow, flag_overflow, flag_NAN
 
 
+@vsc.randobj
+class FPUTransaction:
+    def __init__(self):
+        self.val_A = vsc.rand_bit_t(16)
+        self.val_B = vsc.rand_bit_t(16)
+        self.val_op = vsc.rand_enum_t(ALU_Ops)
+        self.acc = vsc.rand_bit_t(1)
+
+    @vsc.constraint
+    def val_A_rules(self):
+        self.val_A.dist
+        (
+            #10 percent Zero
+            vsc.weight(0x0000, 5),   
+            vsc.weight(0x8000, 5),   
+            
+            #10 percent Infin
+            vsc.weight(0x7F80, 5),   
+            vsc.weight(0xFF80, 5),   
+            
+            #5 percent NAN
+            vsc.weight(vsc.rng(0xFFC0, 0xFFFF), 5),
+            
+            #5 percent 1
+            vsc.weight(0x3F80, 5),
+            
+            #70 percent Normal
+            vsc.weight(vsc.rng(0x0001, 0x3F7F), 70) 
+        ) 
+    
+    def val_B_rules(self):
+        self.val_B.dist
+        (
+            #10 percent Zero
+            vsc.weight(0x0000, 5),   
+            vsc.weight(0x8000, 5),   
+            
+            #10 percent Infin
+            vsc.weight(0x7F80, 5),   
+            vsc.weight(0xFF80, 5),   
+            
+            #5 percent NAN
+            vsc.weight(vsc.rng(0xFFC0, 0xFFFF), 5),
+            
+            #5 percent 1
+            vsc.weight(0x3F80, 5),
+            
+            #70 percent Normal
+            vsc.weight(vsc.rng(0x0001, 0x3F7F), 70) 
+        ) 
+
+
+
 @cocotb.test()
 async def test_project(dut):
 
@@ -82,32 +136,56 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 3)
     dut.reset_n.value = 1
 
+    for _ in range(100):
+        #Driving Stim
+        ##########################################
+        await FallingEdge(dut.clk)
+        txn = FPUTransaction()
+        txn.randomize()
 
-    #Simple test
-    await FallingEdge(dut.clk)
+        val_A = txn.val_A
+        val_B = txn.val_B
+        val_op = int(txn.val_op)
+        val_acc = txn.acc
 
-    val_A = 0x4000
-    val_B = 0x4040
-    val_op = ALU_Ops.MUL
-    val_acc = 0
+        dut.A.value = val_A
+        dut.B.value = val_B
+        dut.op.value = val_op
+        dut.acc.value = val_acc
+        dut.data_ready.value = 1
+        ##########################################
 
-    dut.A.value = val_A
-    dut.B.value = val_B
-    dut.op.value = val_op
-    dut.acc.value = val_acc
-    dut.data_ready.value = 1
-
-    await RisingEdge(dut.clk)
-    await ReadOnly()
-
-    exp_res, exp_uf, exp_of, exp_nan = goldenModel(
-        val_A, val_B, val_op, val_acc, int(dut.accumulate_register.value)
-    )
-
-    hardware_value = int(dut.accumulate_register.value)
+        #Scoreboard
+        ##########################################
+        await RisingEdge(dut.clk)
+        await ReadOnly()
+        dut.data_ready.value = 0
         
-    assert hardware_value == exp_res, f"Math failed! Expected {hex(exp_res)}, Got {hex(hardware_value)}"
-    
-    dut._log.info(f"SUCCESS: Hardware matched Golden Model -> {hex(hardware_value)}")
+        exp_res, exp_uf, exp_of, exp_nan = goldenModel(
+            val_A, val_B, val_op, val_acc, int(dut.accumulate_register.value)
+        )
 
+        hardware_res = int(dut.accumulate_register.value)
+        hardware_uf  = int(dut.fpu_core.flag_underflow) 
+        hardware_of  = int(dut.fpu_core.flag_overflow)
+        hardware_nan = int(dut.fpu_core.flag_NAN)       
+        
+        allTestsPassed = (
+            (hardware_res == exp_res) and 
+            (hardware_uf == exp_uf) and 
+            (hardware_of == exp_of) and 
+            (hardware_nan == exp_nan)
+        )
+
+        assert allTestsPassed, (
+            f"Test Failed! \n"
+            f"Inputs: A={hex(val_A)}, B={hex(val_B)}, OP={hex(val_op)}, ACC={hex(val_acc)}\n"
+            f"Math: Exp {hex(exp_res)}, Got {hex(hardware_res)} \n"
+            f"UF: Exp {exp_uf}, Got {hardware_uf} \n"
+            f"OF: Exp {exp_of}, Got {hardware_of} \n"
+            f"NAN: Exp {exp_nan}, Got {hardware_nan}"
+        )
+        
+        dut._log.info(f"SUCCESS: Hardware matched Golden Model -> {hex(hardware_res)}")    ##########################################
+        ##########################################
 
